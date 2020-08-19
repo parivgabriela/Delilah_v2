6ed3e91b1a5995a0379755fcf278d4a88c1cb129
parivgabriela <pari.gabriela@gmail.com>
2020-08-19 17:53:53 -0300

    se pueden agregar items al carrito con la funcion, verificar el endpoint /pedidos

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sqlite=require('sqlite3');
 const database=require('./DB/db');
 const db=new sqlite.Database('./delilah.sqlite',(err)=>console.log(err));
 const is_admin=1,not_admin=0;
-var id_usuario=6,id_pedido=10,id_carrito=20,id_plato=55;
+var id_usuario=5,id_pedido=10,id_carrito=20,id_plato=51;
 var usuario_activo =-1;//si esta en este valor -1 significa que no hay uno activo
 const api = express();
 api.use(bodyParser.json());
@@ -15,7 +15,9 @@ api.use(bodyParser.json());
 function inicializarDB(){
   db.serialize(function() {
      db.run('DROP TABLE usuarios');
-      
+     db.run('DROP TABLE pedidos');
+     db.run('DROP TABLE platos');
+     db.run('DROP TABLE carritos');
      db.run("CREATE TABLE usuarios ( id_usuario INT PRIMARY KEY NOT NULL, usuario VARCHAR (60) NOT NULL, nombre_apellido VARCHAR (60) NOT NULL, mail VARCHAR(60) NOT NULL, telefono VARCHAR(20) NOT NULL, domicilio VARCHAR (60) NOT NULL, password VARCHAR(20) NOT NULL, is_admin BOOLEAN NOT NULL DEFAULT FALSE)");
      db.run('CREATE TABLE pedidos (id_pedido INT PRIMARY KEY NOT NULL,t_pago VARCHAR (60) NOT NULL,estado_pedido VARCHAR(60) NOT NULL,date DATETIME NOT NULL,total VARCHAR(20) NOT NULL,id_user INT NOT NULL,id_carrito INT NOT NULL)');
      db.run('CREATE TABLE platos (id_plato INT PRIMARY KEY ,nombre_plato VARCHAR (60) NOT NULL,precio FLOAT NOT NULL,url_plato VARCHAR(200) NOT NULL)');
@@ -28,7 +30,7 @@ function inicializarDB(){
           db.all('INSERT INTO usuarios VALUES (3,"4user","Fulani TI","aa@gmail.com","4343456","Juramento 444","1234",FALSE)');
           db.all('INSERT INTO usuarios VALUES (4,"5user","Fulani TO","aa@gmail.com","4343456","Jujuy 444","1234",FALSE)');
 
-          db.all('INSERT INTO platos VALUES (49,"pizza","300","https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg")');
+          db.all('INSERT INTO platos VALUES (50,"pizza","300","https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg")');
 
           db.all('select * from usuarios',(err,resultados)=>{
              console.log(resultados);
@@ -66,6 +68,12 @@ api.post('/usuarios', (req, res, next) => {
 	}
    
 });
+api.post('/usuarios', (req, res, next) => {
+if(!usuario_activo && esAdmin(usuario_activo)){
+
+}
+});
+
 api.get('/login', (req, res,next) => {
   //console.log(req);
   const { usuario, password } = req.body;
@@ -79,25 +87,80 @@ api.get('/login', (req, res,next) => {
   res.status(utils.estadoDeServer.statusOk);
 });
 
-api.get('/platos', (req, res,next)=>{
-  db.serialize(function(){
-    db.run('SELECT * platos',(err,resultados)=>{
+api.get('/platos', (req, res, next) => {
+  db.serialize(function() {
+      db.all('SELECT * from platos', (err, resultados) => {
           console.log(resultados);
-          res.status(utils.mensajeServer.statusOkConsulta);
-          res.status(utils.estadoDeServer.statusOk);
-    })
+      });
   });
+  res.send(utils.mensajeServer.statusOkConsulta);
+  res.status(utils.estadoDeServer.statusOk);
 });
-api.post('/pedido', (req, res,next)=>{
-  const { carrito, t_pago, id_usuario, telefono, domicilio, password } = req.body;
-  db.serialize(function(){
-    db.run('SELECT * platos',(err,resultados)=>{
-          console.log(resultados);
-          res.status(utils.mensajeServer.statusOkConsulta);
-          res.status(utils.estadoDeServer.statusOk);
-    })
+
+//agregar modificacion de admin
+api.post('/platos', (req, res, next) => {
+  const { nombre_plato,precio, url_plato } = req.body;
+  db.serialize(function() {
+    db.run("INSERT INTO platos VALUES (?,?,?,?)",id_plato,nombre_plato, precio,url_plato);
+    id_plato++;
+    res.send(utils.mensajeServer.statusOkConsulta);
+    res.status(utils.estadoDeServer.statusOk);
   });
+
 });
+
+
+api.post('/pedidos', (req, res, next) => {
+  //verificar que el usuario este activo--> se haya logueado
+  const { t_pago, id_usuario, domicilio, total, carrito } = req.body;
+  console.log(t_pago, id_usuario, domicilio, total, carrito);
+  var fecha = new Date();
+  
+  try {
+      db.serialize(function() {
+          db.run('INSERT INTO pedidos VALUES (?,?,?,?,?,?,?)', id_pedido, t_pago, utils.estadoPedidos.pedidoNuevo, fecha, total, (err, resultados) => {
+              id_pedido++;
+          });
+          db.all('INSERT INTO carritos VALUES (?,?,?,?)', id_carrito, id_pedido, carrito[0].id_plato, carrito[0].cantidad,(err,resultados)=>{
+            //if(usuario_o_mail===resultados.usuario || usuario_o_mail===resultados.mail);
+            console.log("Los resultados son "+resultados);
+            console.log(err);
+          id_carrito++;
+        });
+      });
+     // cargarCarrito(carrito);
+      res.status(utils.mensajeServer.statusOkConsulta);
+      res.status(utils.estadoDeServer.statusOk);
+      next();
+  } catch (error) {
+      console.log(error);
+      console.log(utils.estadoDeServer.statusErrorCliente);
+      next(new Error(error));
+  }
+
+});
+api.post('/carritos', (req, res, next) => {
+  const {id_pedido, carrito } = req.body;
+  db.serialize(function() {
+    db.run('INSERT INTO carritos VALUES (?,?,?,?)', 11, 11,11, 1,(err,resultados)=>{
+      console.log("Los resultados son "+resultados);
+      console.log(err);
+    id_carrito++;
+  });
+  });
+})
+function cargarCarrito(unCarrito) {
+  for (let i = 0; i < unCarrito.length; i++) {
+      db.serialize(function() {
+          db.run('INSERT INTO carritos VALUES (?,?,?,?)', id_carrito, id_pedido, carrito[i].id_plato, carrito[i].cantidad,(err,resultados)=>{
+            //if(usuario_o_mail===resultados.usuario || usuario_o_mail===resultados.mail);
+            console.log("Los resultados son "+resultados);
+            console.log(err);
+          id_carrito++;
+        });
+        });
+  }
+}
 function comprobarCuentaIngreso(usuario_o_mail, password) {
   db.serialize(function(){
     db.run('SELECT * FROM usuarios WHERE mail=?',usuario_o_mail,(err,resultados)=>{
@@ -110,4 +173,22 @@ function comprobarCuentaIngreso(usuario_o_mail, password) {
   })
 
 }
+function selectpedidos(){
+  db.serialize(function() {
+    db.all('select * from pedidos',(err,resultados)=>{
+       console.log(resultados);
+   });
+})
+}
+
+function selectCarritos(){
+  db.serialize(function() {
+    db.all('INSERT INTO carritos VALUES (4,4,4,4)');
+
+    db.all('select * from carritos',(err,resultados)=>{
+       console.log(resultados);
+   });
+})
+}
+selectCarritos();
 
